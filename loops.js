40f61a423d8bd9e645485ca97d00d64ef4c5aa63
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-06 21:56:39 -0800

    add worked solution for Fix the Error 1 (4-5)

--- loops.js
+++ loops.js
@@ -179,10 +179,42 @@ while (x >= 1) {
 
 // rewrite the while loop as a for loop
 
-for (let x2 = 9; x2 >= 1; x2 = x2 - 1) {
-  console.log("hello " + x2);
+for (let x = 9; x >= 1; x = x - 1) {
+  console.log("hello " + x);
 }
 
 // even though I wrote x2 in here, on the quiz I had to write it as "x"
+// actually I was wrong - I could use x because it's in different scope,
+// so I changed it back to x instead of x2.
+
+/* ------------------------------------ - ----------------------------------- *1
+
+/* [Quiz: Fix the Error 1 (4-5)]
+Directions:
+Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
+
+for (x < 10; x++) {
+  console.log(x);
+}
+
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `5`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should increment `x` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+// fix the for loop
+
+for (let x = 5; x < 10; x++) {
+  console.log(x);
+}
 
 /* ------------------------------------ - ----------------------------------- */
