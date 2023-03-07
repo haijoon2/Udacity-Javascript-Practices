375fc8ab13671b86363f0918a6b852c18f99c54c
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-06 21:48:55 -0800

    add worked solution for Changing the Loop (4-4)

--- loops.js
+++ loops.js
@@ -153,3 +153,36 @@ while (count >= 0) {
   }
   count -= 1;
 }
+
+/* ------------------------------------ - ----------------------------------- */
+/*
+ * Directions:
+Rewrite the following while loop as a for loop:
+var x = 9;
+while (x >= 1) {
+  console.log("hello " + x);
+  x = x - 1;
+}
+
+ * Programming Quiz: Changing the Loop (4-4)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `9`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should decrement `x` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+// rewrite the while loop as a for loop
+
+for (let x2 = 9; x2 >= 1; x2 = x2 - 1) {
+  console.log("hello " + x2);
+}
+
+// even though I wrote x2 in here, on the quiz I had to write it as "x"
+
+/* ------------------------------------ - ----------------------------------- */
