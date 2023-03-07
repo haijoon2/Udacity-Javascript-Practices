c0c1183fe18545373d100f035872be1b6a2c0315
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-06 22:08:14 -0800

    add worked solution for Quiz Factorials! (4-7)

--- loops.js
+++ loops.js
@@ -218,3 +218,26 @@ for (let x = 5; x < 10; x++) {
 }
 
 /* ------------------------------------ - ----------------------------------- */
+/*
+[QUIZ : FACTORIALS! 4-7)]
+Directions:
+Write a for (note: not a function) loop that prints out the factorial of the number 12:
+
+A factorial is calculated by multiplying a number by all the numbers below it. 
+For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+
+3! = 3 * 2 * 1 = 6
+4! = 4 * 3 * 2 * 1 = 24
+5! = 5 * 4 * 3 * 2 * 1 = 120
+
+Save your final answer in a variable called solution and print it to the console.
+*/
+
+let solution = 1;
+
+for (let x = 12; x > 0; x--) {
+  solution *= x;
+}
+console.log(solution);
+
+/* ------------------------------------ - ----------------------------------- */
