87da11930ab524d8ab350afebaab18f111339ff6
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-07 23:25:30 -0800

    solve Laugh it off 2 (5-2)

--- functions.js
+++ functions.js
@@ -53,3 +53,41 @@ function laugh() {
 }
 console.log(laugh());
 /* ------------------------------------ - ----------------------------------- */
+// [QUIZ] Laugh it Off! (5-2)
+
+/*
+Directions:
+Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return.
+
+console.log(laugh(3));
+
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should have one parameter named `num`
+ * - Your `laugh()` function should return the correct number of laughs
+ */
+
+function laugh2(num) {
+  let laugh2 = "";
+  for (let i = 0; i < num; i++) {
+    laugh2 += "ha";
+  }
+  return laugh2 + "!";
+}
+
+// answer is laugh but the name already exists - so I used laugh2
+
+/* ------------------------------------ - ----------------------------------- */
+// 7. Return Value
+
+// 
