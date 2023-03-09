290b21061b976014a2ae65c18d57928553a46332
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-08 23:01:52 -0800

    solve Quiz Build a Triangle (5-3)

--- functions.js
+++ functions.js
@@ -88,6 +88,37 @@ function laugh2(num) {
 // answer is laugh but the name already exists - so I used laugh2
 
 /* ------------------------------------ - ----------------------------------- */
-// 7. Return Value
+// [QUIZ] Build a Triangle (5-3)]
+// /*
+ * Programming Quiz: Build A Triangle (5-3)
+*/
 
-// 
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have a `buildTriangle()` function
+* - Your `buildTriangle()` function should take one argument (or you can say parameter)
+* - Your `laugh()` function should build the triangle as describe above
+*/
+
+
+// creates a line of * for a given length
+function makeLine(length) {
+   var line = "";
+   for (var j = 1; j <= length; j++) {
+       line += "* ";
+   }
+   return line + "\n";
+}
+
+// your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(height) {
+  let lines = "";
+  
+  for (let i = 1; i <= height; i++) {
+      lines += makeLine(i);
+  }
+  return lines;
+}
+
+// test your code by uncommenting the following line
+console.log(buildTriangle(10));
