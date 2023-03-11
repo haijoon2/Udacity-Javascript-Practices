a8cca568916fedb5adf425247f641f18bff7f6de
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-11 10:03:13 -0800

    Solve Udacity Objects Quiz: Facebook Friends (7-5)

--- objects.js
+++ objects.js
@@ -94,3 +94,51 @@ const savingsAccount = {
 
 console.log(savingsAccount.printAccountSummary());
 /* ------------------------------------ - ----------------------------------- */
+/* Quiz: Facebook Friends (7-5)
+
+Directions:
+Create an object called facebookProfile. The object should have 3 properties:
+
+1.your name
+2.the number of friends you have, and
+3.an array of messages you've posted (as strings)
+
+The object should also have 4 methods:
+
+1.postMessage(message) - adds a new message string to the array
+2.deleteMessage(index) - removes the message corresponding to the index provided
+3.addFriend() - increases the friend count by 1
+4.removeFriend() - decreases the friend count by 1
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `facebookProfile`
+ * - The `facebookProfile` object should have the `name` (string), `friends` (number), and `messages` (array of strings) property
+ * - Your `facebookProfile` object should have the `postMessage()`, `deleteMessage()`, `addFriend()` and `removeFriend()` method
+ * - Carefully implement the desired functionality of each method, as decribed above
+ */
+
+// TIP -
+// In an array,
+// - addition at the end is done using push() method
+// - addition at a specific index is done using splice() method
+// - deletion from the beginning is done using pop() method
+// - deletion from a specific index is done using splice() method
+
+// your code goes here
+const facebookProfile = {
+  name: "Haijoon Kim",
+  friends: 10,
+  messages: ["hello", "happy birthday", "haha!"],
+  postMessage: function (message) {
+    facebookProfile.messages.push(message);
+  },
+  deleteMessage: function (index) {
+    facebookProfile.messages.splice(index, 1);
+  },
+  addFriend: function () {
+    facebookProfile.friends++;
+  },
+  removeFriend: function () {
+    facebookProfile.friends--;
+  },
+};
+/* ------------------------------------ - ----------------------------------- */
